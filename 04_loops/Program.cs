9aa571d0d72ac9829e563a91088f2a385a0213f1
tasspolatt <tasspolatt@gmail.com>
2024-10-13 13:58:42 +0300

    Add files via upload

---
+++ 04_loops/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+            //döngüler
+            //For(x;y;z)
+            //x:başlangıç değeri
+            //y:bitiş
+            //z:artış*-azalış
+
+
+
+            //int i;
+
+            //for (i = 1;  i <= 5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim");
+            //}
+
+            //for (int i = 3; i <= 50; i+=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen Ekrana Yazılmasını İStediginiz Adedi Giriniz");
+            //int finishValue = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("Adam");
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+            #region For Döngüsü İle karar Yapıları
+
+            //for (int i = 0; i <= 100; i++)
+
+            //{ if (i % 5 == 0)
+
+            //    { Console.WriteLine(i);
+
+
+            //    }
+
+
+
+            //}
+            //int totalValue = 0;
+            //for(int i = 1; i <= 10; i++) { totalValue += i; }
+            //Console.WriteLine(totalValue);
+
+
+            //int totalValue = 0;
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+
+            //    }
+            //}
+            //Console.WriteLine("--------");
+            //Console.WriteLine(totalValue);
+            //    int count = 0;
+            //    for (int i = 1; i <= 50; i++)
+            //    {
+            //       if (i % 7 == 0 ) 
+            //        count++;
+            //    }
+
+            //Console.WriteLine(count);
+
+
+            //int bacterium = 1;
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ".Saat Sonunda:  " + bacterium);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region While Döngüsü
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+
+
+
+
+            //}
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+
+            //    { Console.WriteLine(i); }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+            //while (i <=10)
+            //{
+            //    sum += i;
+            //    i++;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Örnek Sınav Sorusu
+            // kalvyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız
+
+            //456
+            Console.Write("Sayıyı Giriniz; ");
+            int number = int.Parse(Console.ReadLine());
+            int ones, tens, hundreds;
+            int sum;
+            ones = number % 10;
+            tens = (number % 100) / 10;
+            hundreds =number / 100;
+            Console.WriteLine(ones + "-"+tens+"-"+hundreds);
+            sum=ones+tens+hundreds;
+            Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
